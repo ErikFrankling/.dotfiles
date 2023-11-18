432fa27d4f200da5de2d7d41366d461e080f93f9
ErikFrankling <erik.frankling@frankling.se>
2023-11-18 04:58:56 +0100

    completed vnc laptop config

--- scripts/connect_vnc.ps1
+++ scripts/connect_vnc.ps1
@@ -1,2 +1,3 @@
-ssh -v -L 5900:localhost:5900 erikf@arch-erik-pc WLR_BACKENDS=headless WLR_LIBINPUT_NO_DEVICES=1 sway -V
-C:\Program -fullscreen Files\TightVNC\tvnviewer.exe localhost::5900
+start-process -FilePath "ssh" -ArgumentList ("-vL", "5900:localhost:5900", "erikf@arch-erik-pc", "WLR_BACKENDS=headless WLR_LIBINPUT_NO_DEVICES=1 sway -V")
+Start-Sleep -Seconds 3
+& "C:\Users\erikf\projects\.dotfiles\other\laptop-vnc-viewer-config.vnc" 
