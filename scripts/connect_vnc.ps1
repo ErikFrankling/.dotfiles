a97dce127b61270041d8dac890b7f6600e53d945
ErikFrankling <erik.frankling@frankling.se>
2024-01-01 18:25:41 +0100

    remote

--- scripts/connect_vnc.ps1
+++ scripts/connect_vnc.ps1
@@ -1,3 +1,4 @@
-start-process -FilePath "ssh" -ArgumentList ("-vL", "5900:localhost:5900", "erikf@arch-erik-pc", "WLR_BACKENDS=headless WLR_LIBINPUT_NO_DEVICES=1 sway -V")
-Start-Sleep -Seconds 3
+start-process -FilePath "ssh" -ArgumentList ("-vLfN", "5900:localhost:5900", "erikf@192.168.0.232")#, "WLR_BACKENDS=headless WLR_LIBINPUT_NO_DEVICES=1 sway -V")
+start-process -FilePath "ssh" -ArgumentList ("-v", "erikf@192.168.0.232", "WAYLAND_DISPLAY=wayland-1 wayvnc -Ldebug --output DP-1")
+Start-Sleep -Seconds 5
 & "C:\Users\erikf\projects\.dotfiles\other\laptop-vnc-viewer-config.vnc" 
