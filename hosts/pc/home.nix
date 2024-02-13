8098cb737f6496370bcbc87bdcf13941eff11f75
ErikFrankling <erik.frankling@frankling.se>
2024-02-13 13:31:22 +0100

    test

--- hosts/pc/home.nix
+++ hosts/pc/home.nix
@@ -44,6 +44,8 @@
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
 
+    ".config/nvim/".source = .dotfiles/nvim;
+
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
