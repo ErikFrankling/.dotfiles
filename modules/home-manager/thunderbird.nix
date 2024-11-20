70679a79935d8d8a8f2f97c4e5383ffdada12e46
Erik Frankling <erik.frankling@frankling.se>
2024-11-20 22:12:54 +0100

    sync

---
+++ modules/home-manager/thunderbird.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, lib, ... }:
+
+let
+  signature = {
+    text = "Mvh Erik Frankling";
+    showSignature = "append";
+  };
+in
+{
+  programs.thunderbird = {
+    enable = true;
+    # profiles.erikf = {
+    #   isDefault = true;
+    # };
+  };
+
+  accounts.email.accounts.erikf = {
+    realName = "Erik Frankling";
+    address = "erik.frankling@frankling.se";
+    userName = "erik.frankling";
+    # primary = true;
+    imap = {
+      host = "outlook.office365.com";
+      port = 993;
+    };
+    # passwordCommand = "cat ${config.age.secrets.microsoft.path}";
+    thunderbird = {
+      enable = true;
+      # profiles = [ "erikf" ];
+    };
+    inherit signature;
+  };
+
+  imports = [
+  ];
+
+  home.packages = with pkgs; [
+  ];
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # ".config/nvim/" = {
+    #   source = ../../nvim;
+    #   recursive = true;
+    # };
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    # EDITOR = lib.mkForce "/home/erikf/.nix-profile/bin/nvim";
+  };
+}
--- modules/nixos/nm.nix
+++ modules/nixos/nm.nix
@@ -1,86 +1,93 @@
 { config, pkgs, lib, ... }:
 
-with lib;
-
-let
-  cfg = config.networking.networkmanager;
-
-  getFileName = stringAsChars (x: if x == " " then "-" else x);
-
-  createWifi = ssid: opt: {
-    name = ''
-      NetworkManager/system-connections/${getFileName ssid}.nmconnection
-    '';
-    value = {
-      mode = "0400";
-      source = pkgs.writeText "${ssid}.nmconnection" ''
-        [connection]
-        id=${ssid}
-        type=wifi
-
-        [wifi]
-        ssid=${ssid}
-
-        [wifi-security]
-        ${optionalString (opt.psk != null) ''
-        key-mgmt=wpa-psk
-        psk=${opt.psk}''}
-      '';
-    };
-  };
-
-  keyFiles = mapAttrs' createWifi config.networking.wireless.networks;
-in
 {
-  config = mkIf cfg.enable {
-    environment.systemPackages = [
-    ];
 
-    networking.networkmanager.wifi.powersave = true;
-
-    networking.wireless.enable = false;
-    environment.etc = keyFiles;
-
-    systemd.services.NetworkManager-predefined-connections = {
-      restartTriggers = mapAttrsToList (name: value: value.source) keyFiles;
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "${pkgs.coreutils}/bin/true";
-        ExecReload = "${pkgs.networkmanager}/bin/nmcli connection reload";
-      };
-      reloadIfChanged = true;
-      wantedBy = [ "multi-user.target" ];
-    };
-  };
+  environment.systemPackages = [
+    pkgs.wpa_supplicant_gui
+  ];
+  # networking.wireless.dbusControlled = true;
+  networking.networkmanager.enable = lib.mkForce false;
+  networking.wireless.enable = true;
+  networking.wireless.userControlled.enable = true;
+  users.extraUsers.erikf.extraGroups = [ "wheel" ];
+  #
+  # sops.secrets."wireless.env" = { };
+  # networking.wireless.secretsFile = config.sops.secrets."wireless.env".path;
+  #
+  # networking.wireless.networks = {
+  #   "Galaxy S22 Erik" = {
+  #     pskRaw = "ext:s22_psk";
+  #   };
+  #   eduroam = {
+  #     # pskRaw = "ext:eduroam_psk";
+  #     auth = ''
+  #       key_mgmt=WPA-EAP
+  #       eap=PWD
+  #       identity="ext:eduroam_identity"
+  #       password="ext:eduroam_psk"
+  #     '';
+  #   };
+  # };
 }
 
-
-
-
-
-
-# networking.wireless.dbusControlled = true;
-# networking.networkmanager.enable = lib.mkForce false;
-# networking.wireless.enable = true;
-# networking.wireless.userControlled.enable = true;
-# users.extraUsers.erikf.extraGroups = [ "wheel" ];
+# { config, pkgs, lib, ... }:
 #
-# sops.secrets."wireless.env" = { };
-# networking.wireless.secretsFile = config.sops.secrets."wireless.env".path;
+# with lib;
 #
-# networking.wireless.networks = {
-#   "Galaxy S22 Erik" = {
-#     pskRaw = "ext:s22_psk";
-#   };
-#   eduroam = {
-#     # pskRaw = "ext:eduroam_psk";
-#     auth = ''
-#       key_mgmt=WPA-EAP
-#       eap=PWD
-#       identity="ext:eduroam_identity"
-#       password="ext:eduroam_psk"
+# let
+#   cfg = config.networking.networkmanager;
+#
+#   getFileName = stringAsChars (x: if x == " " then "-" else x);
+#
+#   createWifi = ssid: opt: {
+#     name = ''
+#       NetworkManager/system-connections/${getFileName ssid}.nmconnection
 #     '';
+#     value = {
+#       mode = "0400";
+#       source = pkgs.writeText "${ssid}.nmconnection" ''
+#         [connection]
+#         id=${ssid}
+#         type=wifi
+#
+#         [wifi]
+#         ssid=${ssid}
+#
+#         [wifi-security]
+#         ${optionalString (opt.psk != null) ''
+#         key-mgmt=wpa-psk
+#         psk=${opt.psk}''}
+#       '';
+#     };
 #   };
-# };
-}
+#
+#   keyFiles = mapAttrs' createWifi config.networking.wireless.networks;
+# in
+# {
+#   config = mkIf cfg.enable {
+#     environment.systemPackages = [
+#     ];
+#
+#     networking.networkmanager.wifi.powersave = true;
+#
+#     networking.wireless.enable = false;
+#     environment.etc = keyFiles;
+#
+#     systemd.services.NetworkManager-predefined-connections = {
+#       restartTriggers = mapAttrsToList (name: value: value.source) keyFiles;
+#       serviceConfig = {
+#         Type = "oneshot";
+#         RemainAfterExit = true;
+#         ExecStart = "${pkgs.coreutils}/bin/true";
+#         ExecReload = "${pkgs.networkmanager}/bin/nmcli connection reload";
+#       };
+#       reloadIfChanged = true;
+#       wantedBy = [ "multi-user.target" ];
+#     };
+#   };
+# }
+
+
+
+
+
