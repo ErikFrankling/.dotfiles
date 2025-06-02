d7418b0b6707dda24f4dec024bbf77e36eac96a7
Erik Frankling <erik.frankling@frankling.se>
2025-06-02 18:25:49 +0200

    openscad-unstable rolled back

--- modules/home-manager/print/default.nix
+++ modules/home-manager/print/default.nix
@@ -1,7 +1,29 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }: 
+
+# let
+#     # pkgs = import (builtins.fetchTarball {
+#     #     url = "https://github.com/NixOS/nixpkgs/archive/c792c60b8a97daa7efe41a6e4954497ae410e0c1.tar.gz";
+#     #     sha256 = "sha256:1kbdg5ziq6w4jls5wjvp4ddpww39zzm0pr8chz97qdj1l19pa7zn";
+#     # }) {};
+#
+#     pkgs = import (builtins.fetchGit {
+#          # Descriptive name to make the store path easier to identify
+#          name = "my-old-revision";
+#          url = "https://github.com/NixOS/nixpkgs/";
+#          ref = "refs/heads/nixpkgs-unstable";
+#          rev = "c792c60b8a97daa7efe41a6e4954497ae410e0c1";
+#      }) {};
+#
+#     openscad-2024 = pkgs.openscad-unstable;
+# in
+{
+
   home.packages = with pkgs; [
     prusa-slicer
-    openscad-unstable
+    openscad
+    # openscad-2024
+    # TODO: use openscad-unstable when it is no longer broken. it has much faster rendering
+    # openscad-unstable
   ];
   # home.file = {
   #   ".config/PrusaSlicer/" = {
