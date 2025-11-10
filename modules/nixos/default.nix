4db14366a104c201260f047b2060b86f5c2b02d7
erik.frankling@frankling.se <Erik Frankling>
2025-11-10 12:03:21 +0100

    ai + vpn

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -20,9 +20,9 @@
     ./ibus.nix
     ./krb5.nix
   ];
-  programs.nix-ld.enable = true;
 
   programs.nix-ld.enable = true;
+
   nix.settings.download-buffer-size = 524288000;
   nix = {
     gc = {
