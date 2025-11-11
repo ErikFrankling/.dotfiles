06d15fab209250b4877fd7398065b6cb3db857fe
erik.frankling@frankling.se <Erik Frankling>
2025-11-11 05:51:20 +0100

    broken update + ai

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -21,16 +21,23 @@
 
   home.packages = with pkgs; [
     webcord
-    otherPkgs.pkgsMaster.spotifywm
+    # otherPkgs.pkgsMaster.spotifywm
+    spotifywm
     # spotify-qt
     pavucontrol
     google-chrome
     # gparted
     # virtualbox
     syncthingtray-minimal
-    obsidian
+    # FIX: role this back. was causing electron rebuild.
+    otherPkgs.pkgsStable.obsidian
+    # obsidian
     # (inputs.ags-shell.packages.${system}.my-shell)
     inputs.ags-shell.packages."x86_64-linux".default
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-36.9.5"
   ];
 
   services.syncthing.tray.enable = true;
