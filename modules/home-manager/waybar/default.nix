7480d71f9419c29bf8565f7451a808d3ef07f1b6
Erik Frankling <erik.frankling@frankling.se>
2026-04-20 05:54:50 +0200

    waybar fix

--- modules/home-manager/waybar/default.nix
+++ modules/home-manager/waybar/default.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [ jq ];
 
   home.file = {
-    ".config/waybar/cofig.jsonc".source = ./config.jsonc;
+    ".config/waybar/config.jsonc".source = ./config.jsonc;
     ".local/bin/gpu-util.sh" = {
       source = ./scripts/gpu-util.sh;
       executable = true;
@@ -17,7 +17,7 @@
 
   programs.waybar = {
     enable = true;
-    # systemd.enable = true;
+    systemd.enable = true;
 
     style = ./style.css;
     # settings = builtins.readFile ./config.jsonc;
