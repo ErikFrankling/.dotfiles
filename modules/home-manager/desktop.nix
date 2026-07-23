f8ea869ff627df7cbe7b3ab55f14d9dae336471d
Erik Frankling <erik.frankling@frankling.se>
2026-07-24 01:06:57 +0200

    enforce dark desktop theme

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -69,4 +69,16 @@
       -t "C,," \
       -i ${../../certificates/cloudflare-gateway-ca.pem}
   '';
+  gtk = {
+    enable = true;
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+  };
+
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    gtk-theme = "adw-gtk3-dark";
+  };
 }
